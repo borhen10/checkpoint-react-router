14d3fbe96f3cda52ff090ff7ca70f3a2b6ad9968
borhen10 <73178497+borhen10@users.noreply.github.com>
2021-01-16 00:40:52 +0100

    checkpoint-react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {Data} from './data';
+import {MovieList} from './Component/MovieList';
+import {FilterV2} from './Component/FilterV2';
+import {useState} from 'react';
+import Trailer from './Component/Trailer';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
+function App(props) {
+  const [anime, setAnime] = useState(Data);
+  const [rateValue, setRateValue] = useState(5);
+  const [inputFilter, setInputFilter] = useState('');
+  const filterHandler = (searchTerm) => {
+    setInputFilter(searchTerm);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <FilterV2 ratevalue={rateValue} setratevalue={setRateValue} searching={filterHandler}/>
+        <Route
+          path="/anime/:id"
+          render={(props) => <Trailer {...props} animelist={anime} />}
+        />
+        <MovieList animelist={anime.filter(el => el.rate <= rateValue && el.title.toLocaleLowerCase().includes(inputFilter.toLocaleLowerCase()))} setanime={setAnime}/>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Component/MovieCard.js
@@ -0,0 +1,25 @@
+import { Rate } from 'antd';
+import 'antd/dist/antd.css';
+import { Link} from 'react-router-dom';
+
+
+
+export const MovieCard = ({item}) => {
+    return (
+        <>
+        <Link to={`/anime/${item.title}`}>
+            <div className='anime'>
+                <img src={item.posterUrl} alt={item.title}></img>
+                <div className='anime-info'>
+                    <h2>{item.title}</h2>
+                    <Rate disabled value={parseInt(item.rate)} style={{fontSize: 25}}/>
+                </div>
+                <div className='anime-description'>
+                    <h2>Description :</h2>
+                    <p>{item.description}</p>
+                </div>
+            </div>
+        </Link>
+        </>
+    );
+}
